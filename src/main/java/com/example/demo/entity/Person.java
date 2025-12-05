655e0ffdb65804f303b028ee0ef7f2c448a5abaa
emanzat <emanzat@gmail.com>
2025-12-05 13:38:04 +0100

    to workflow

--- src/main/java/com/example/demo/entity/Person.java
+++ src/main/java/com/example/demo/entity/Person.java
@@ -4,12 +4,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "#{@environment.getProperty('mongodb.collection.person')}")
-public record Person(
-    @Id String id,
-    String name,
-    int age
-) {
-    public Person(String name, int age) {
-        this(null, name, age);
-    }
+public record Person(@Id String id, String name, int age) {
+	public Person(String name, int age) {
+		this(null, name, age);
+	}
 }
